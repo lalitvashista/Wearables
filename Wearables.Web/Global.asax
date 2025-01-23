d704c5e022457df5737e9e9af9f73fc0c7a86749
Lalit Vashishtha <lalit.kvashishta@gmail.com>
2025-01-23 20:51:33 +0530

    Project initiated

---
+++ Wearables.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Wearables.Web.MvcApplication" Language="C#" %>
---
+++ Wearables.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wearables.Web.Startup))]
+namespace Wearables.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
