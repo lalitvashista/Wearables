b6b5def473dcd5da713688033ea39cd3cba5f7c7
Lalit Vashishtha <lalit.kvashishta@gmail.com>
2025-02-04 21:42:48 +0530

    Added product controller

---
+++ Wearables.Services/ProductsService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wearables.Database;
+using Wearables.Entities;
+
+namespace Wearables.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int ID)
+        {
+            using (var context = new WContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var context = new WContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var context = new WContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new WContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new WContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Wearables.Web/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wearables.Entities;
+using Wearables.Services;
+
+namespace Wearables.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable()
+        {
+            var products = productsService.GetProducts();
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
